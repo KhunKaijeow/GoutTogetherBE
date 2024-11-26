6fdd078fcf9947e62f5e1461c3f544c2a2f2d446
KhunKaijeow <khunkaijeow@gmail.com>
2024-11-26 13:59:09 +0700

    Rename file

--- src/main/java/dev/kjcoder/goutbackend/tourcompany/dto/RegisterTourCompanyDto.java
+++ src/main/java/dev/kjcoder/goutbackend/tourcompany/dto/RegisterTourCompanyDto.java
@@ -1,10 +1,12 @@
-package dev.kjcoder.goutbackend.tourcompany.model;
+package dev.kjcoder.goutbackend.tourcompany.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterTourCompanyDto(
         Integer tourCompanyId,
         @NotBlank String tourCompanyName,
+        @NotBlank String username,
+        @NotBlank String password,
         String status
 ) {
 }
