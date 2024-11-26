992ad28c4bc44a0cba5ad1b768a1a8a0ba4cfe4c
KhunKaijeow <khunkaijeow@gmail.com>
2024-11-26 14:00:04 +0700

    Add TourCompanyWalletRepository

---
+++ src/main/java/dev/kjcoder/goutbackend/tourcompany/repository/TourCompanyWalletRepository.java
@@ -0,0 +1,7 @@
+package dev.kjcoder.goutbackend.tourcompany.repository;
+
+import dev.kjcoder.goutbackend.tourcompany.model.TourCompanyWallet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourCompanyWalletRepository extends CrudRepository<TourCompanyWallet, Integer> {
+}
