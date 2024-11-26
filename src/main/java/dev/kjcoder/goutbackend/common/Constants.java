72c3e166ccc7acdf4b30224a1b3a783f24d49c9e
KhunKaijeow <khunkaijeow@gmail.com>
2024-11-26 14:39:16 +0700

    Add constant value for role

---
+++ src/main/java/dev/kjcoder/goutbackend/common/Constants.java
@@ -0,0 +1,13 @@
+package dev.kjcoder.goutbackend.common;
+
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final String ROLE_CONSUMER = "ROLE_CONSUMER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_COMPANY = "ROLE_COMPANY";
+
+}
