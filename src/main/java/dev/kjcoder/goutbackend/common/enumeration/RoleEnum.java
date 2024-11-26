e3bea7fbcc47b4deeaa4c1ee54951a565f66ed88
KhunKaijeow <khunkaijeow@gmail.com>
2024-11-26 11:40:20 +0700

    Add enum for role

--- src/main/java/dev/kjcoder/goutbackend/common/enumeration/RoleEnum.java
+++ src/main/java/dev/kjcoder/goutbackend/common/enumeration/RoleEnum.java
@@ -1,13 +1,13 @@
 package dev.kjcoder.goutbackend.common.enumeration;
 
-public enum Role {
+public enum RoleEnum {
     CONSUMER(1),
     ADMIN(2),
     COMPANY(3);
 
     private final int id;
 
-    Role(int id) {
+    RoleEnum(int id) {
         this.id = id;
     }
 
