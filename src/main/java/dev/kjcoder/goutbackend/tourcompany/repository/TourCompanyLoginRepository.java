9475e1f1fda098dbe141afaa159c155c35974909
KhunKaijeow <khunkaijeow@gmail.com>
2024-11-26 13:59:43 +0700

    Add TourCompanyLoginRepository

---
+++ src/main/java/dev/kjcoder/goutbackend/tourcompany/repository/TourCompanyLoginRepository.java
@@ -0,0 +1,7 @@
+package dev.kjcoder.goutbackend.tourcompany.repository;
+
+import dev.kjcoder.goutbackend.tourcompany.model.TourCompanyLogin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourCompanyLoginRepository extends CrudRepository<TourCompanyLogin, Integer> {
+}
